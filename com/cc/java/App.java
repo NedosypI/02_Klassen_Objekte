c8649b1bc736d2e37984d3d9a6aa1f501846ff10
NedosypI <irinanedosyp@gmx.de>
2022-07-20 11:04:25 +0200

    Update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,6 +1,22 @@
 package com.cc.java;
 
 public class App {
+
+    public static void main(String[] args) {
+
+        // Verweis auf Klassen oder Klassen Großgeschriebenn Variablen klein
+//Referenz von Aussen = unser Name -> REFERENZVARIABLE
+      Cat cat = new Cat(); // wir bilden aus einer Klasse ein Objekt
+      System.out.println("Blick von aussen: " + cat);
+      cat.tellYourAdress();
+
+
+      Cat cat1 = new Cat(); 
+      System.out.println("Blick von aussen: " + cat1);
+      cat1.tellYourAdress();
+
+        // System.out.println("Hi");
+    }
     
    
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class Cat {
+    
+    public void tellYourAdress() {
+        System.out.println("Blick von innen: " + this); //this= Selbstreferenz
+    // //Referenz von innen = das "Ich" = Selbstreferenz -> INSTANZVARIABLE
+    
+
+
+    }
+
+
+
+
+    }
+
